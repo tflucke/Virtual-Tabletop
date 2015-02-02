984a05968e7a66041fe40d48567e29a86c5b44ae
Tom Flucke <tomflucke@gmail.com>
2015-02-02 05:21:26 +0000

    Uses debug print statements.

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -8,25 +8,35 @@
 #include <iostream>			/* std::cout, std::endl */
 #include "network/ServerSocket.h"	/* ServerSocket */
 #include "network/SocketException.h"	/* SocketException */
+#include "debug/DebugMethods.hpp"	/* printIfDebug, VERBOSE*/
 //#include "clients/Client.h"		/* Client */
 
 int
 main (int argsLength, char** args)
 {
+  debugMode = VERBOSE;
   try
   {
-    ServerSocket ss (18081);
+    printIfDebug (VERBOSE, "Creating server socket.");
+    ServerSocket ss (3345);
     TCPSocket* tcps1 = ss.accept ();
+    printIfDebug (VERBOSE, "Received first connection.");
     TCPSocket* tcps2 = ss.accept ();
-    tcps1->print(tcps2->readLine());
-    tcps2->print(tcps1->readLine());
+    printIfDebug (VERBOSE, "Received second connection.");
+    printIfDebug (VERBOSE, "Sending Con2 private IP to Con1.");
+    tcps1->print (tcps2->readLine ());
+    printIfDebug (VERBOSE, "Sending Con1 private IP to Con2.");
+    tcps2->print (tcps1->readLine ());
+    printIfDebug (VERBOSE, "Sending Con2 public IP to Con1.");
     tcps1->print ("%s:%d\n", tcps2->getClientIP (), tcps2->getClientPort ());
+    printIfDebug (VERBOSE, "Sending Con1 public IP to Con2.");
     tcps2->print ("%s:%d\n", tcps1->getClientIP (), tcps1->getClientPort ());
   }
   catch (SocketException& se)
   {
-    std::cout << se.what () << std::endl;
+    printIfDebug (FATAL, se.what ());
   }
+  printIfDebug (VERBOSE, "Exiting.");
   return 0;
 }
 //
