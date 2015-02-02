ba6b651fcce8e53998233b4724df59b2707f1c6e
Tom Flucke <tomflucke@gmail.com>
2015-02-02 05:06:10 +0000

    Added debug prints and changed default port.

--- client/src/testprograms/HolePunchNetworkHostTestor.java
+++ client/src/testprograms/HolePunchNetworkHostTestor.java
@@ -33,6 +33,7 @@ import name.tomflucke.components.AutoCompleteComboBox;
 import name.tomflucke.dragNdrop.DragDropListener;
 import name.tomflucke.layouts.TableLayout;
 import name.tomflucke.network.HolePuncher;
+import name.tomflucke.virtualdesktop.DebugConstants;
 import name.tomflucke.virtualdesktop.map.RPGMap;
 import name.tomflucke.virtualdesktop.map.RPGMap.Layer;
 import name.tomflucke.virtualdesktop.map.Tile;
@@ -44,225 +45,303 @@ import name.tomflucke.virtualdesktop.ui.layerlist.ColumnConstants;
 import name.tomflucke.virtualdesktop.ui.layerlist.ColumnInfo;
 import name.tomflucke.virtualdesktop.ui.layerlist.LayerTable;
 
-public class HolePunchNetworkHostTestor implements ColumnConstants {
+public class HolePunchNetworkHostTestor implements ColumnConstants,
+        DebugConstants
+{
 	private static final InetSocketAddress serverAddress = new InetSocketAddress(
-			"63.246.2.164", 18081);
-
+	        "63.246.2.164", 3345);
+	// "127.0.0.1", 3345);
+	
+	private static final byte DEBUG_MODE = VERBOSE;
 	private static RPGMap map;
 	private static MapDisplay mapPanel;
 	private static LayerTable layerList;
 	private static ObjectOutputStream client;
-
-	private static JFrame buildWindow() {
+	
+	private static JFrame buildWindow()
+	{
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(685, 510);
 		window.getContentPane().setLayout(
-				new TableLayout(new double[][] {
-						{ TableLayout.FILL, TableLayout.PREFERRED },
-						{ TableLayout.PREFERRED, TableLayout.PREFERRED,
-								TableLayout.FILL, TableLayout.FILL } }));
+		        new TableLayout(new double[][] {
+		                { TableLayout.FILL, TableLayout.PREFERRED },
+		                { TableLayout.PREFERRED, TableLayout.PREFERRED,
+		                        TableLayout.FILL, TableLayout.FILL } }));
 		return window;
 	}
-
-	private static JMenuBar buildMenuBar() {
+	
+	private static JMenuBar buildMenuBar()
+	{
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
 		menuBar.add(fileMenu);
 		JMenuItem saveButton = new JMenuItem("Save");
 		fileMenu.add(saveButton);
-		saveButton.addActionListener(new ActionListener() {
+		saveButton.addActionListener(new ActionListener()
+		{
 			@Override
-			public void actionPerformed(ActionEvent ae) {
-				try {
+			public void actionPerformed(ActionEvent ae)
+			{
+				try
+				{
 					File f = new File("/home/tom/testMap.map");
 					f.delete();
 					ObjectOutputStream oos = new ObjectOutputStream(
-							new FileOutputStream(f));
+					        new FileOutputStream(f));
 					oos.writeObject(map);
 					oos.close();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
 		});
 		JMenuItem loadButton = new JMenuItem("Load");
 		fileMenu.add(loadButton);
-		loadButton.addActionListener(new ActionListener() {
+		loadButton.addActionListener(new ActionListener()
+		{
 			@Override
-			public void actionPerformed(ActionEvent ae) {
-				try {
+			public void actionPerformed(ActionEvent ae)
+			{
+				try
+				{
 					ObjectInputStream ois = new ObjectInputStream(
-							new FileInputStream("/home/tom/testMap.map"));
+					        new FileInputStream("/home/tom/testMap.map"));
 					map = (RPGMap) ois.readObject();
 					ois.close();
 					mapPanel.setMap(map);
 					layerList.setMap(map);
 					System.out.println(map.getLayers().length);
-				} catch (IOException | ClassNotFoundException e) {
+				}
+				catch (IOException | ClassNotFoundException e)
+				{
 					e.printStackTrace();
 				}
 			}
 		});
 		return menuBar;
 	}
-
-	private static MapDisplay buildMapEditor(RPGMap map) {
+	
+	private static MapDisplay buildMapEditor(RPGMap map)
+	{
 		MapDisplay mapPanel = new MapEditor();
 		mapPanel.setMap(map);
 		return mapPanel;
 	}
-
-	private static JScrollPane buildMapPane(MapDisplay mapPanel) {
+	
+	private static JScrollPane buildMapPane(MapDisplay mapPanel)
+	{
 		JPanel mapContainer = new JPanel();
 		mapContainer.add(mapPanel);
 		JScrollPane mapScrollPane = new JScrollPane(mapContainer);
 		return mapScrollPane;
 	}
-
-	private static JList<Tile> buildTileList(MapDisplay mapPanel) {
+	
+	private static JList<Tile> buildTileList(MapDisplay mapPanel)
+	{
 		JList<Tile> tileList = new JList<Tile>();
 		tileList.setCellRenderer(new TileListRenderer());
-		DragDropListener<Tile> tileDragger = new DragDropListener<Tile>() {
+		DragDropListener<Tile> tileDragger = new DragDropListener<Tile>()
+		{
 			@Override
-			public Tile drag(Component comp, Point p) {
+			public Tile drag(Component comp, Point p)
+			{
 				return (Tile) ((JList<?>) comp).getSelectedValue();
 			}
-
+			
 			@Override
-			public void drop(Component comp, Point p, Tile data) {
+			public void drop(Component comp, Point p, Tile data)
+			{
 				((MapEditor) comp).addLayer(p, data);
 			}
-
+			
 			@Override
-			public void endDrag() {
+			public void endDrag()
+			{
 			}
 		};
 		tileDragger.register(DragDropListener.DRAG_ZONE, tileList);
 		tileDragger.register(DragDropListener.DROP_ZONE, mapPanel);
 		return tileList;
 	}
-
+	
 	private static AutoCompleteComboBox buildTileGroupSelector(
-			JList<Tile> tileList) {
+	        JList<Tile> tileList)
+	{
 		AutoCompleteComboBox tileGroupSelector = new AutoCompleteComboBox(
-				Arrays.asList(Tile.getGroupNames()));
-		tileGroupSelector.addItemListener(new ItemListener() {
+		        Arrays.asList(Tile.getGroupNames()));
+		tileGroupSelector.addItemListener(new ItemListener()
+		{
 			@Override
-			public void itemStateChanged(ItemEvent ie) {
+			public void itemStateChanged(ItemEvent ie)
+			{
 				tileList.setListData(Tile.getGroup((String) ie.getItem()));
 			}
-
+			
 		});
 		tileList.setListData(Tile.getGroup((String) tileGroupSelector
-				.getSelectedItem()));
+		        .getSelectedItem()));
 		return tileGroupSelector;
 	}
-
-	private static LayerTable buildLayerList(RPGMap map) {
+	
+	private static LayerTable buildLayerList(RPGMap map)
+	{
 		LayerTable layerList = new LayerTable(new ColumnInfo[] {
-				new ColumnInfo(40, HIDE_BUTTON), new ColumnInfo(75, PREVIEW),
-				new ColumnInfo(70, LAYER_NAME) });
+		        new ColumnInfo(40, HIDE_BUTTON), new ColumnInfo(75, PREVIEW),
+		        new ColumnInfo(70, LAYER_NAME) });
 		layerList.setMap(map);
 		return layerList;
 	}
-
-	private static Socket connectToPeer() throws IOException {
+	
+	private static Socket connectToPeer() throws IOException
+	{
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Initalizing socket.");
 		Socket server = new Socket();
 		server.setReuseAddress(true);
 		server.setTcpNoDelay(true);
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Connecting to server.");
 		server.connect(serverAddress);
 		int port = server.getLocalPort();
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating output stream.");
 		PrintWriter out = new PrintWriter(server.getOutputStream());
+		DebugConstants
+		        .printIfDebug(DEBUG_MODE, VERBOSE, "Printing private IP.");
 		out.println(InetAddress.getLocalHost().getHostAddress() + ":" + port);
 		out.flush();
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating input stream.");
 		BufferedReader in = new BufferedReader(new InputStreamReader(
-				server.getInputStream()));
+		        server.getInputStream()));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Reading remote private IP.");
 		String[] ip_port = in.readLine().split(":");
 		InetSocketAddress localPeerSocket = new InetSocketAddress(ip_port[0],
-				Integer.valueOf(ip_port[1]));
+		        Integer.valueOf(ip_port[1]));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Reading remote public IP.");
 		ip_port = in.readLine().split(":");
 		InetSocketAddress remotePeerSocket = new InetSocketAddress(ip_port[0],
-				Integer.valueOf(ip_port[1]));
+		        Integer.valueOf(ip_port[1]));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Closing server connection.");
 		server.close();
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Punching hole.");
 		return HolePuncher.punchTCP(remotePeerSocket, localPeerSocket, port);
 	}
-
-	private static MapListener buildMapListener() {
-		return new MapListener() {
-			private void updateLayer(int id, Object obj) {
-				try {
+	
+	private static MapListener buildMapListener()
+	{
+		return new MapListener()
+		{
+			private void updateLayer(int id, Object obj)
+			{
+				try
+				{
 					client.writeInt(id);
 					client.writeObject(obj);
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
-
+			
 			@Override
-			public void layerAdded(Layer newLayer) {
+			public void layerAdded(Layer newLayer)
+			{
 				updateLayer(-1, newLayer);
 			}
-
+			
 			@Override
-			public void layerRemoved(Layer removedLayer) {
+			public void layerRemoved(Layer removedLayer)
+			{
 				updateLayer(removedLayer.getLayerId(), null);
 			}
-
+			
 			@Override
-			public void layerMoved(Layer changedLayer) {
+			public void layerMoved(Layer changedLayer)
+			{
 				updateLayer(changedLayer.getLayerId(),
-						changedLayer.getPosition());
+				        changedLayer.getPosition());
 			}
-
+			
 			@Override
-			public void layerResized(Layer changedLayer) {
+			public void layerResized(Layer changedLayer)
+			{
 				updateLayer(changedLayer.getLayerId(), changedLayer.getSize());
 			}
-
+			
 			@Override
-			public void layerReordered(Layer changedLayer) {
+			public void layerReordered(Layer changedLayer)
+			{
 				updateLayer(changedLayer.getLayerId(),
-						Integer.valueOf(changedLayer.getZPosition()));
+				        Integer.valueOf(changedLayer.getZPosition()));
 			}
-
+			
 			@Override
-			public void layerRenamed(Layer changedLayer) {
+			public void layerRenamed(Layer changedLayer)
+			{
 			}
-
+			
 			@Override
-			public void layerVisibilitySet(Layer changedLayer) {
+			public void layerVisibilitySet(Layer changedLayer)
+			{
 				updateLayer(changedLayer.getLayerId(),
-						Boolean.valueOf(changedLayer.isVisible()));
+				        Boolean.valueOf(changedLayer.isVisible()));
 			}
-
+			
 		};
 	}
-
-	public static void main(final String... args) throws IOException {
+	
+	public static void main(final String... args) throws IOException
+	{
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Creating Window.");
 		JFrame window = buildWindow();
 		window.setJMenuBar(buildMenuBar());
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Creating map.");
 		map = new RPGMap(new Dimension(6, 6));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Adding Grass tile.");
 		map.addLayer(Tile.getTile("Grass"), new Point(1, 2),
-				new Dimension(1, 1));
+		        new Dimension(1, 1));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Adding Water tile.");
 		map.addLayer(Tile.getTile("Water"), new Point(1, 2),
-				new Dimension(2, 2));
-
+		        new Dimension(2, 2));
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating map display.");
 		mapPanel = buildMapEditor(map);
 		window.add(buildMapPane(mapPanel), "0, 0, 0, 3");
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Creating tile list.");
 		JList<Tile> tileList = buildTileList(mapPanel);
 		window.add(tileList, "1, 2");
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating tile group drop down list.");
 		window.add(buildTileGroupSelector(tileList), "1, 1");
-
+		
+		DebugConstants
+		        .printIfDebug(DEBUG_MODE, VERBOSE, "Creating layer list.");
 		layerList = buildLayerList(map);
 		window.add(layerList, "1, 3");
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Displaying window.");
 		window.setVisible(true);
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Connecting to peer and creating output stream.");
 		client = new ObjectOutputStream(connectToPeer().getOutputStream());
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Sending map info.");
+		client.writeObject(map);
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating map listener.");
 		map.addMapListener(buildMapListener());
 	}
 }
--- client/src/testprograms/HolePunchNetworkPeerTestor.java
+++ client/src/testprograms/HolePunchNetworkPeerTestor.java
@@ -16,93 +16,163 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import name.tomflucke.network.HolePuncher;
+import name.tomflucke.virtualdesktop.DebugConstants;
 import name.tomflucke.virtualdesktop.map.RPGMap;
 import name.tomflucke.virtualdesktop.map.RPGMap.Layer;
 import name.tomflucke.virtualdesktop.ui.MapDisplay;
 import name.tomflucke.virtualdesktop.ui.layerlist.ColumnConstants;
 
-public class HolePunchNetworkPeerTestor implements ColumnConstants {
+public class HolePunchNetworkPeerTestor implements ColumnConstants,
+        DebugConstants
+{
 	private static final InetSocketAddress serverAddress = new InetSocketAddress(
-			"63.246.2.164", 18081);
-
+			"63.246.2.164", 3345);
+	//		"127.0.0.1", 3345);
+	
+	private static final byte DEBUG_MODE = VERBOSE;
 	private static RPGMap map;
 	private static MapDisplay mapPanel;
 	private static Socket server;
-
-	private static Socket connectToPeer() throws IOException {
+	
+	private static Socket connectToPeer() throws IOException
+	{
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Initalizing socket.");
 		Socket server = new Socket();
 		server.setReuseAddress(true);
 		server.setTcpNoDelay(true);
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Connecting to server.");
 		server.connect(serverAddress);
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating output stream.");
 		int port = server.getLocalPort();
 		PrintWriter out = new PrintWriter(server.getOutputStream());
+		DebugConstants
+		        .printIfDebug(DEBUG_MODE, VERBOSE, "Printing private IP.");
 		out.println(InetAddress.getLocalHost().getHostAddress() + ":" + port);
 		out.flush();
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating input stream.");
 		BufferedReader in = new BufferedReader(new InputStreamReader(
-				server.getInputStream()));
+		        server.getInputStream()));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Reading remote private IP.");
 		String[] ip_port = in.readLine().split(":");
 		InetSocketAddress localPeerSocket = new InetSocketAddress(ip_port[0],
-				Integer.valueOf(ip_port[1]));
+		        Integer.valueOf(ip_port[1]));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Reading remote public IP.");
 		ip_port = in.readLine().split(":");
 		InetSocketAddress remotePeerSocket = new InetSocketAddress(ip_port[0],
-				Integer.valueOf(ip_port[1]));
+		        Integer.valueOf(ip_port[1]));
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Closing server connection.");
 		server.close();
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Punching hole.");
 		return HolePuncher.punchTCP(remotePeerSocket, localPeerSocket, port);
 	}
-
-	private static JFrame buildWindow() {
+	
+	private static JFrame buildWindow()
+	{
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(685, 510);
 		return window;
 	}
-
-	private static MapDisplay buildMapDisplay(RPGMap map) {
+	
+	private static MapDisplay buildMapDisplay(RPGMap map)
+	{
 		MapDisplay mapPanel = new MapDisplay();
 		mapPanel.setMap(map);
 		return mapPanel;
 	}
-
-	private static JScrollPane buildMapPane(MapDisplay mapPanel) {
+	
+	private static JScrollPane buildMapPane(MapDisplay mapPanel)
+	{
 		JPanel mapContainer = new JPanel();
 		mapContainer.add(mapPanel);
 		JScrollPane mapScrollPane = new JScrollPane(mapContainer);
 		return mapScrollPane;
 	}
-
+	
 	public static void main(final String... args) throws IOException,
-			ClassNotFoundException {
+	        ClassNotFoundException
+	{
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Connecting to Host.");
 		server = connectToPeer();
+		DebugConstants
+		        .printIfDebug(DEBUG_MODE, VERBOSE, "Saving input stream.");
 		ObjectInputStream in = new ObjectInputStream(server.getInputStream());
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Reading map info.");
 		map = (RPGMap) in.readObject();
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Creating Window.");
 		JFrame window = buildWindow();
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+		        "Creating map display.");
 		mapPanel = buildMapDisplay(map);
 		window.add(buildMapPane(mapPanel));
-
+		
+		DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Displaying window.");
 		window.setVisible(true);
-
-		while (!server.isClosed()) {
+		
+		while (!server.isClosed())
+		{
+			DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+			        "Connection still open.");
+			
+			DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Read layer id.");
 			int id = in.readInt();
+			DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+			        "Read layer object.");
 			Object obj = in.readObject();
-			if (id < 0) {
+			if (id < 0)
+			{
+				DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE,
+				        "Layer Added.\nRepainting.");
 				mapPanel.revalidate();
-			} else {
+			}
+			else
+			{
 				Layer l = map.getLayerById(id);
-				if (obj == null) {
+				if (obj == null)
+				{
+					DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Layer "
+					        + id + " deleted.");
 					// Remove Layer
-				} else if (obj instanceof Point) {
-					l.setPosition((Point) obj);
-				} else if (obj instanceof Dimension) {
-					l.setSize((Dimension) obj);
-				} else if (obj instanceof Boolean) {
-					l.setVisible((Boolean) obj);
-				} else if (obj instanceof Integer) {
-					l.setZPosition((Integer) obj);
+				}
+				else if (obj instanceof Point)
+				{
+					Point pos = (Point) obj;
+					DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Layer "
+					        + id + " moved to " + pos + ".");
+					l.setPosition(pos);
+				}
+				else if (obj instanceof Dimension)
+				{
+					Dimension size = (Dimension) obj;
+					DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Layer "
+					        + id + " resized to +" + size + ".");
+					l.setSize(size);
+				}
+				else if (obj instanceof Boolean)
+				{
+					boolean visible = (Boolean) obj;
+					DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Layer "
+					        + id + " visibility set to " + visible + ".");
+					l.setVisible(visible);
+				}
+				else if (obj instanceof Integer)
+				{
+					int zIndex = (Integer) obj;
+					DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Layer "
+					        + id + " reordered to " + zIndex + ".");
+					l.setZPosition(zIndex);
 				}
 			}
+			DebugConstants.printIfDebug(DEBUG_MODE, VERBOSE, "Repainting.");
 			mapPanel.repaint();
 		}
 	}
