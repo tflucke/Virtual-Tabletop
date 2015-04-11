fdbf2fb83ad6bc4267ab4fc98bd23f127ab3e53a
Tom Flucke <tomflucke@gmail.com>
2015-04-11 03:37:19 +0000

    Switching to boost libs.

--- server/src/UDPTestServer.cpp
+++ server/src/UDPTestServer.cpp
@@ -5,42 +5,57 @@
  *      Author: Tom Flucke
  */
 
-#include <iostream>						/* std::cout, std::endl */
-#include "network/UDPSocket.h"			/* ServerSocket */
-#include "network/SocketException.h"	/* SocketException */
-#include "debug/DebugMethods.hpp"		/* printIfDebug, VERBOSE*/
+#include <boost/asio.hpp>
+#include <iostream>
 
 int
 main (int argsLength, char** args)
 {
-	  debugMode = VERBOSE;
-	  try
-	  {
-	    printIfDebug (VERBOSE, "Creating server socket.");
-	    UDPSocket socket (3345);
-	    //while (true)
-	    //{
-	    	UDPSocket::IPAddress ip1 = socket.read()->sender;
-			printIfDebug (VERBOSE, "Received first connection.");
-			printIfDebug (VERBOSE, "Con1: %s:%u.", ip1.ip, ip1.port);
-			socket.print(ip1, "Hello World!");
-//			UPDSocket::IPAddress ip2 = socket.read().sender;
-//			printIfDebug (VERBOSE, "Received second connection.");
-//			printIfDebug (VERBOSE, "Con2: %s:%u.", ip2.ip, ip2.port);
-//	        printIfDebug (VERBOSE, "Sending Con2 private port to Con1.");
-//
-//	        printIfDebug (VERBOSE, "Sending Con1 private port to Con2.");
-//
-//	        printIfDebug (VERBOSE, "Sending Con2 public port to Con1.");
-//	        socket.print(ip1, "%s:%d", ip2.ip, ip2.port);
-//	        printIfDebug (VERBOSE, "Sending Con1 public port to Con2.");
-//	        socket.print(ip2, "%s:%d", ip1.ip, ip1.port);
-	    //}
-	  }
-	  catch (SocketException& se)
-	  {
-	    printIfDebug (FATAL, se.what ());
-	  }
-	  printIfDebug (VERBOSE, "Exiting.");
-	  return 0;
+	try
+	{
+		boost::asio::io_service io;
+		//udp_server server(io);
+		io.run();
+		return 0;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 }
+//int
+//main (int argsLength, char** args)
+//{
+//	  debugMode = VERBOSE;
+//	  try
+//	  {
+//	    printIfDebug (VERBOSE, "Creating server socket.");
+//	    UDPSocket socket (3345);
+//	    //while (true)
+//	    //{
+//	    	UDPSocket::IPAddress* ip1 = socket.read()->sender;
+//			printIfDebug (VERBOSE, "Received first connection.");
+//			//printIfDebug (VERBOSE, "Con1: %s:%d.", ip1->getIpString(), ip1->getPort());
+//			printf("Con1: %s:%d.\n", ip1->getIpString(), ip1->getPort());
+//			socket.print(ip1, "%s:%d", ip1->getIpString(), ip1->getPort());
+////			UPDSocket::IPAddress ip2 = socket.read().sender;
+////			printIfDebug (VERBOSE, "Received second connection.");
+////			printIfDebug (VERBOSE, "Con2: %s:%u.", ip2.ip, ip2.port);
+////	        printIfDebug (VERBOSE, "Sending Con2 private port to Con1.");
+////
+////	        printIfDebug (VERBOSE, "Sending Con1 private port to Con2.");
+////
+////	        printIfDebug (VERBOSE, "Sending Con2 public port to Con1.");
+////	        socket.print(ip1, "%s:%d", ip2.ip, ip2.port);
+////	        printIfDebug (VERBOSE, "Sending Con1 public port to Con2.");
+////	        socket.print(ip2, "%s:%d", ip1.ip, ip1.port);
+//	    //}
+//	  }
+//	  catch (SocketException& se)
+//	  {
+//	    printIfDebug (FATAL, se.what ());
+//	  }
+//	  printIfDebug (VERBOSE, "Exiting.");
+//	  return 0;
+//}
